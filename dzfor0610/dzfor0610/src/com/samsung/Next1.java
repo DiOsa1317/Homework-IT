847c2ddc3e8023ad16a5ca1baaf8c6502cf70f0f
DiOsa1317 <dilusha.37.writer@gmail.com>
2022-10-06 18:22:38 +0300

    1.5.1.1

--- dzfor0610/dzfor0610/src/com/samsung/Next1.java
+++ dzfor0610/dzfor0610/src/com/samsung/Next1.java
@@ -6,7 +6,7 @@ public class Next1 {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             double x = sc.nextDouble();
-            boolean aBool=(x>=3 && x<=5) ;
+            boolean aBool=(x>=3 && x<=8) ;
             System.out.println(aBool);
         }
     }
