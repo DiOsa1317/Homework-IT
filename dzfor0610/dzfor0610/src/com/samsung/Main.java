87098d654b2519b80a5468166d5ca00d9320e785
DiOsa1317 <dilusha.37.writer@gmail.com>
2022-10-06 17:36:08 +0300

    1.5.0.1

--- dzfor0610/dzfor0610/src/com/samsung/Main.java
+++ dzfor0610/dzfor0610/src/com/samsung/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         double x = sc.nextDouble();
         double y = sc.nextDouble();
-        String res = (y<Math.sin(x) && y<0.5 && y>0 && x>0 && x<Math.PI) ? "YES" : "NO";
+        String res = (x * x + y * y > 4.0 && x < 2.0 && x > y && y > 0.0) ? "YES" : "NO";
         System.out.println(res);
     }
 }
