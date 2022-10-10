7ab0dc22ead36aa88461c0ca9b0c02570bed5fd9
DiOsa1317 <dilusha.37.writer@gmail.com>
2022-10-10 20:35:34 +0300

    1.5.0.7

---
+++ dzfor0610/dzfor0610/src/com/samsung/Next8.java
@@ -0,0 +1,15 @@
+package com.samsung;
+
+import java.util.Scanner;
+
+public class Next8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+        String res = (x<1 && y>1-x && y>2*x*x) || (x>=0.5 && x<1 && y>1-x) ? "YES" : "NO";
+        System.out.println(res);
+    }
+}
+
+}
