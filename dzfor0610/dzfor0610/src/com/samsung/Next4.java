34d30aeb556e0d0fc4c4ae9e9871e5fb37fb2f22
DiOsa1317 <dilusha.37.writer@gmail.com>
2022-10-10 19:14:20 +0300

    Update Next4.java

--- dzfor0610/dzfor0610/src/com/samsung/Next4.java
+++ dzfor0610/dzfor0610/src/com/samsung/Next4.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Next4 {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            double x = sc.nextDouble();
-            boolean aBool=x>0 && x%100!=0 && x%5==0;
+            int x = sc.nextInt();
+            boolean aBool=x>0 && x/100>0 && x/100<10 && x%5==0;
             System.out.println(aBool);
         }
     }
