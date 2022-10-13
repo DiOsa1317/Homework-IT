97e825b89a8610f43854d5dfa4087710b3e07e07
DiOsa1317 <dilusha.37.writer@gmail.com>
2022-10-13 17:20:07 +0300

    1.7.0.3

---
+++ dzfor131022/src/com/samsung/Main3.java
@@ -0,0 +1,20 @@
+package com.samsung;
+
+import java.util.Scanner;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int s=0, a, b, n;
+        n=sc.nextInt();
+        while(n>=10 && n<=99) {
+            a=n%10;
+            b=n/10;
+            s=s+a+b;
+            n=sc.nextInt();
+        }
+        System.out.println(s);
+    }
+}
+
+}
