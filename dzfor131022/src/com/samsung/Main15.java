d8cb0bedd88df64f8cf43b9a259fe419a0b78779
DiOsa1317 <dilusha.37.writer@gmail.com>
2022-10-15 22:08:09 +0300

    1.7.1.5

---
+++ dzfor131022/src/com/samsung/Main15.java
@@ -0,0 +1,16 @@
+package com.samsung;
+
+import java.util.Scanner;
+
+public class Main15 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int s=0;
+        while(N>0){
+            s=s+N%10;
+            N=N/10;
+        }
+        System.out.println(s);
+    }
+}
