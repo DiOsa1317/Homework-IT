916ea27657fe7966e16767870252fc8bbd7df4a4
DiOsa1317 <dilusha.37.writer@gmail.com>
2022-10-24 20:25:27 +0300

    1.8.2.7

---
+++ dzfor18102022/src/com/samsung/Main21.java
@@ -0,0 +1,18 @@
+package com.samsung;
+
+import java.util.Scanner;
+
+public class Main21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] ar = new int[N];
+        int i, k = 1;
+        ar[0] = sc.nextInt();
+        for (i = 1; i < ar.length; i++) {
+            ar[i] = sc.nextInt();
+            if (ar[i] > ar[i - 1]) k++;
+        }
+        System.out.println(k);
+    }
+}
