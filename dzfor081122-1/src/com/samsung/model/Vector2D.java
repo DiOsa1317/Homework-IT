c461ab742b53751dc5c7fd7acd383f0b23830907
DiOsa1317 <dilusha.37.writer@gmail.com>
2022-11-10 18:27:39 +0300

    task2

--- dzfor081122-1/src/com/samsung/model/Vector2D.java
+++ dzfor081122-1/src/com/samsung/model/Vector2D.java
@@ -3,22 +3,26 @@ package com.samsung.model;
 import java.util.Locale;
 
 public class Vector2D {
+    public static int count=0;
     public double vX;
     public double vY;
 
     public Vector2D() {
         vX=1;
         vY=1;
+        count++;
     }
 
     public Vector2D(double x, double y) {
         this.vX=x;
         this.vY=y;
+        count++;
     }
 
     public Vector2D(Vector2D vector2D) {
         this.vX=vector2D.vX;
         this.vY=vector2D.vY;
+        count++;
     }
 
     public void print() {
@@ -37,5 +41,20 @@ public class Vector2D {
     public void sub(Vector2D v) {
         this.vX=vX-v.vX;
         this.vY=vY-v.vY;
+    }
+
+    public void scale(double scaleFactor) {
+        this.vX=vX*scaleFactor;
+        this.vY=vY*scaleFactor;
+    }
+
+    public void normalized(){
+        double m=this.length();
+        this.vX=vX/m;
+        this.vY=vY/m;
+    }
+
+    public double dotProduct(Vector2D v) {
+        return vX*v.vX+vY*v.vY;
     }
 }
