315a2c6ed29591e402560c4bd621241f4a54caa0
DiOsa1317 <dilusha.37.writer@gmail.com>
2022-11-27 19:16:02 +0300

    старое

---
+++ dzfor081122-3/src/com/samsung/App.java
@@ -0,0 +1,17 @@
+package com.samsung;
+
+import com.samsung.model.Fraction;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        Fraction[] a = new Fraction[n];
+        for(int i=0; i<n; i++) {
+
+        }
+    }
+}
---
+++ dzfor081122-3/src/com/samsung/model/Fraction.java
@@ -0,0 +1,7 @@
+package com.samsung.model;
+
+public class Fraction {
+ private int num=1;
+ private int denom=1;
+
+}
